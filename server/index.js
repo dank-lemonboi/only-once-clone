123ff7fc758af3fd6734345a87247dd572f8327c
dank-lemonboi <nictayt@gmail.com>
2018-04-23 21:40:02 -0600

    setting up SCSS pre-compiler

---
+++ server/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express'),
+      sessions = require('express-session'),
+      massive = require('massive'),
+      bodyParser = require('body-parser'),
+      cors = require('cors')
+
+      const {
+          SERVER_PORT,
+          SESSION_SECRET,
+          CONNECTION_STRING
+      } = process.env
+
+const app = express()
+
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use(sessions({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}))
+
+massive(CONNECTION_STRING).then( db => {
+    app.set('db', db)
+    app.listen( SERVER_PORT, () => console.log(`It's going down on port ${SERVER_PORT}!`))
+})
+
+
